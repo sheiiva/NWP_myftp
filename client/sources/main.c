f07a98dbcda7c7d3659505a432ab49deb050827b
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-04-13 19:36:16 +0200

    [ADD] server --> main

--- client/sources/main.c
+++ client/sources/main.c
@@ -10,8 +10,8 @@
 int main(int ac, char **av)
 {
     if (ac != 3 || ((ac == 3) && (!atoi(av[2])))) {
-        write(2, "Wrong argument.", 16);
-        write(1, "Please enter the server's port.", 32);
+        write(2, "Wrong argument.\n", 17);
+        write(1, "Please enter the server's port.\n", 33);
         return (84);
     }
     return client(av[1], atoi(av[2]));
