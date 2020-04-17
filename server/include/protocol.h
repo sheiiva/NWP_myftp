9f28dc98b1b8ade62aa1d597320aacb9af901761
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-04-17 03:09:22 +0200

    [ADD] protocol's maccros

---
+++ server/include/protocol.h
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2019
+** SERVER // INCLUDE
+** File description:
+** protocol.h
+*/
+
+#ifndef PROTOCOL_H_
+    #define PROTOCOL_H_
+
+    #define READYFORNMIN        "120"
+    #define TRANSFERSTART       "125"
+    #define OPENDATACONNECT     "150"
+    #define COMMANDOK           "200"
+    #define HELP MESSAGE        "214"
+    #define READYFORNEWUSER     "220"
+    #define LOGOUT              "221"
+    #define CLOSEDATACONNECT    "226"
+    #define PASSIVEMODE         "227"
+    #define LOGGEDIN            "230"
+    #define ACTIONOK            "250"
+    #define PATHNAMECREATED     "257"
+    #define USERNAMEOK          "331"
+    #define NEEDACCOUNT         "332"
+    #define ERROR               "XXX"
+
+#endif /* !PROTOCOL_H_ */
