62af91f453b977030789e9c8cd657ab283381d75
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-04-18 00:50:22 +0200

    [UPDATE] cmd --> CONNECT+PASS

--- server/sources/commands/cmd_connect.c
+++ server/sources/commands/cmd_connect.c
@@ -11,11 +11,17 @@
 int cmd_user(server_t *server, client_t *client)
 {
     (void)server;
-
-    printf("User name okay, need password.\n");
-    if (dprintf(client->fd, "%s\n", USERNAMEOK) < 0) {
-        perror("cmd_connect.c :: Send 331 Reply-code");
-        return (84);
+    if (!strcmp(server->buffer + strlen("USER "), "Anonymous")) {
+        printf("User name okay, need password.\n");
+        if (dprintf(client->fd, "%s\n", USERNAMEOK) < 0) {
+            perror("cmd_connect.c :: Send 331 Reply-code");
+            return (84);
+        }
+    } else {
+        if (dprintf(client->fd, "%s\n", ERROR) < 0) {
+            perror("cmd_connect.c :: Send ERROR Reply-code");
+            return (84);
+        } 
     }
     return (0);
 }
@@ -23,11 +29,17 @@ int cmd_user(server_t *server, client_t *client)
 int cmd_pass(server_t *server, client_t *client)
 {
     (void)server;
-    if (dprintf(client->fd, "%s\n", LOGGEDIN) < 0) {
-        perror("cmd_connect.c :: Send 331 Reply-code");
-        return (84);
-    } else
-    printf("User logged in, proceed.\n");
-    client->connected = true;
-    return (0);
+    if (!strcmp(server->buffer, "PASS ")) {
+        if (dprintf(client->fd, "%s\n", LOGGEDIN) < 0) {
+            perror("cmd_connect.c :: Send 331 Reply-code");
+            return (84);
+        } else
+        printf("User logged in, proceed.\n");
+        client->connected = true;
+    } else {
+        if (dprintf(client->fd, "%s\n", ERROR) < 0) {
+            perror("cmd_connect.c :: Send ERROR Reply-code");
+            return (84);
+        } 
+    }    return (0);
 }
