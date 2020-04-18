6a3dfd285db8058e9142d60f2ce0e571ca28f464
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-04-18 10:26:11 +0200

    [UPDATE] cmd --> PWD: now check for user's log status

--- server/sources/commands/cmd_path.c
+++ server/sources/commands/cmd_path.c
@@ -27,10 +27,18 @@ int cmd_cdup(server_t *server, client_t *client)
 int cmd_pwd(server_t *server, client_t *client)
 {
     (void)server;
-    printf("%s\n", client->path);
-    if (dprintf(client->fd, "%s\n", PATHNAMECREATED) < 0) {
-        perror("cmd_help.c :: Send 257 Reply-code");
-        return (84);
+    if (client->connected) {
+        printf("%s\n", client->path);
+        if (dprintf(client->fd, "%s\n", PATHNAMECREATED) < 0) {
+            perror("cmd_help.c :: Send 257 Reply-code");
+            return (84);
+        }
+    } else {
+        printf("Please loggin to access data.\n");
+        if (dprintf(client->fd, "%s\n", ERROR) < 0) {
+            perror("cmd_help.c :: Send ERROR Reply-code");
+            return (84);
+        } 
     }
     return (0);
 }
