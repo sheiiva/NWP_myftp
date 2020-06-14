4fd8f2831f750200228080bfc18c85c421dbeec6
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-06-14 18:06:00 +0200

    [ADD] client need to connect to noop

--- server/sources/commands/cmd_noop.c
+++ server/sources/commands/cmd_noop.c
@@ -9,6 +9,8 @@
 
 int noop(server_t *server, int index)
 {
+    if (server->clients[index].state == DISCONNECTED)
+        return (write_to(server->clients[index].sockfd, NOTLOGGEDIN));
     if (write_to(server->clients[index].sockfd, COMMANDOK) == 84)
         return (FAILURE);
     return (SUCCESS);
